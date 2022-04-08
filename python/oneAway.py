d1006f82b0b10f4432a3de93ef3353d94928acb5
ericlove02 <53005525+ericlove02@users.noreply.github.com>
2022-04-08 12:34:21 -0500

    finished one away

--- python/oneAway.py
+++ python/oneAway.py
@@ -1,5 +1,3 @@
-# NOT COMPLETE
-
 def one_away(str1, str2):
     """1.5: given two strings, write a function to check if they are one or zero
     edits away
@@ -9,23 +7,38 @@ def one_away(str1, str2):
     """
     # an edit can insert, remove, or replace a character
     # str1 and str2 will have at most 1 character different
-    if len(str1) < len(str2):
-        temp = str1
-        str1 = str2
-        str2 = temp
+    if(len(str1) == len(str2)):
+        return oneReplace(str1, str2)
+    elif(len(str1) + 1 == len(str2)):
+        return oneInsert(str1, str2)
+    elif(len(str1) - 1 == len(str2)):
+        return oneInsert(str2, str1)
+    return False
 
-    edits = 0
+
+def oneReplace(str1, str2):
+    foundDiff = False
     for i in range(len(str1)):
-        if i >= len(str2):
-            edits += 1
-        elif not str1[i] == str2[i] or str1[i+1] == str2[i]:
-            edits += 1
+        if not str1[i] == str2[i]:
+            if(foundDiff):
+                return False
+            foundDiff = True
+    return True
 
-    print(edits)
-    if edits > 1:
-        return False
-    else:
-        return True
+
+def oneInsert(str1, str2):
+    i1 = 0
+    i2 = 0
+    while(i1 < len(str1) and i2 < len(str2)):
+        if not str1[i1] == str2[i2]:
+            if i1 != i2:
+                return False
+            i2 += 1
+        else:
+            i1 += 1
+            i2 += 1
+    return True
+
 
 in1 = str(input("str1: "))
 in2 = str(input("str2: "))
